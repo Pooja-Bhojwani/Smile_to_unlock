579ee49e47f91502242d4ea500f7faa122fbd9e7
Pooja-Bhojwani <Bhojwani.pooja@ymail.com>
2017-08-13 13:10:50 -0400

    Unwanted statements
    Removing unwanted comments

--- image_commons.py
+++ image_commons.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[ ]:
-
 
 """
 This module contains functions used to manipulate images in OpenCV and PIL's Image.
@@ -19,8 +15,6 @@ else:
 def image_as_nparray(image):
     """
     Converts PIL's Image to numpy's array.
-    :param image: PIL's Image object.
-    :return: Numpy's array of the image.
     """
     return np.asarray(image)
 
@@ -28,9 +22,6 @@ def image_as_nparray(image):
 def nparray_as_image(nparray, mode='RGB'):
     """
     Converts numpy's array of image to PIL's Image.
-    :param nparray: Numpy's array of image.
-    :param mode: Mode of the conversion. Defaults to 'RGB'.
-    :return: PIL's Image containing the image.
     """
     return Image.fromarray(np.asarray(np.clip(nparray, 0, 255), dtype='uint8'), mode)
 
@@ -38,8 +29,6 @@ def nparray_as_image(nparray, mode='RGB'):
 def load_image(source_path):
     """
     Loads RGB image and converts it to grayscale.
-    :param source_path: Image's source path.
-    :return: Image loaded from the path and converted to grayscale.
     """
     source_image = cv2.imread(source_path)
     return cv2.cvtColor(source_image, cv2.COLOR_BGR2GRAY)
